146b20024b966689f2cd27608d57f775a8697295
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-06-06 02:58:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
 # code here!
+
+class School 
+  attr_reader :name 
+  attr_accessor :roster
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end 
+  
+  def add_student(name, grade)
+    roster["grade"]=[]
+    roster["grade"].push(name)
+  end 
+end 
+
